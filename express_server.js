87ac1a93c888f8eaaa077e5aa12e37dd2c0ab161
Emily <emilymnicholas@gmail.com>
2020-05-11 23:41:58 +0000

    adding shortURL handler

--- express_server.js
+++ express_server.js
@@ -13,13 +13,29 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
-app.get("/urls.json", (req, res) => {
+app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
+
+app.get('/urls', (req, res) => {
+  let templateVar = { urls: urlDatabase };
+  res.render('url_index', templateVar);
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  let templateVar = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render('url_shows', templateVar)
+})
+
+//render command will serve the template to index.ejs
+//sending back to the browser an html file that has been produced by the template
+//start simple and build up - understand from the beginning and what it does
+
+// express.js documentation - returning things to the front end
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
